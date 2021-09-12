565dbdd4988c19d856d54a8d55b70d0b60c81b05
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-09-12 20:12:13 +0300

    front end oqla

---
+++ src/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+//from login.js
+class Profile extends Component {
+  render() {
+    return (
+      
+          <>
+            <div>Hello {user.name}</div>
+            <div>email : {user.email}</div>
+          </>
+
+    );
+  }
+}
+
+export default Profile;
